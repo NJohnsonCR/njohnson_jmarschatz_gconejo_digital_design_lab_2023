ccf60eecc7baba4302aa6d2ee2ad4115658f4218
gabriel conejo <gacova95@gmail.com>
2023-09-10 19:31:44 -0600

    actualizacion alu
    se actuliza el case de la alu para que llame a los modulos

--- Lab#3/alu.sv
+++ Lab#3/alu.sv
@@ -1,41 +1,37 @@
 
 
-module alu(
-           input [7:0] A,B,  // ALU 8-bit Inputs
+module alu #(parameter N=4)(
+           input [N-1:0] A,B,  // ALU 8-bit Inputs
            input [3:0] ALU_Sel,// ALU Selection
-           output [7:0] ALU_Out, // ALU 8-bit Output
-           output CarryOut // Carry Out Flag
+           output [N-1:0] ALU_Out, // ALU 8-bit Output
+           output FlagZ,FlagN,FlagV,FlagC // Antonio
     );
-    reg [7:0] ALU_Result;
-    wire [8:0] tmp;
-    assign ALU_Out = ALU_Result; // ALU out
-    assign tmp = {1'b0,A} + {1'b0,B};
-    assign CarryOut = tmp[8]; // Carryout flag
+    
     always @()
     begin
         case(ALU_Sel)
         4'b0000: // suma
-           ALU_Result = A + B ; 
+           Suma #(.N(M)) sum(.A(A),.B(B),.FlagC(FlagC),.FlagZ(FlagZ),.FlagZ(FlagZ),.C(ALU_Out));
         4'b0001: // resta
            ALU_Result = A - B ;
         4'b0010: // Multiplicacion
-           ALU_Result = A B;
+           Multiplicacion #(.N(M)) mul(.A(A),.B(B),.FlagZ(FlagZ),.FlagV(FlagV),.C(ALU_Out));
         4'b0011: // Division
-           ALU_Result = A/B;
+           Division #(.N(M)) div(.A(A),.B(B),.FlagZ(.FlagZ),.FlagN(FlagN),.FlagV(FlagV),.FlagC(FlagC),.C(ALU_Out));
         4'b0100: // Modulo
-          ALU_Result = A%B; 
+           Modulo #(.N(M)) div(.A(A),.B(B),.FlagZ(.FlagZ),.FlagN(FlagN),.FlagV(FlagV),.FlagC(FlagC),.C(ALU_Out));
          4'b0101: // and
-          ALU_Result = A & B;
+          Op_AND #(.N(M)) opAnd(.A(A),.B(B),.C(ALU_Out));
          4'b0110: // or
-           ALU_Result = A | B;
+           Op_OR #(.N(M)) opOr(.A(A),.B(B),.C(ALU_Out));
          4'b0111: // xor
-          ALU_Result = A ^ B;
+          Op_XOR #(.N(M)) opXor(.A(A),.B(B),.C(ALU_Out));
           4'b1000: //  shift left 
-           ALU_Result = A<<1;
+           ShiftLeft #(.N(M)) lsh(.A(A),.B(B),.resultado(ALU_Out),.FlagZ(FlagZ));
           4'b1001: //  shift right
-           ALU_Result = A>>1;
+           ShiftRight #(.N(M)) rsh(.A(A),.B(B),.resultado(ALU_Out),.FlagZ(FlagZ));
 
-          default: ALU_Result = A + B ; 
+          default: ALU_Result = 1 ; 
         endcase
     end
 
