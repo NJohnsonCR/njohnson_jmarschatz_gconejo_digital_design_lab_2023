a61d28d089378f81bd39528514eb5c2d759532cb
JanM <jmarschatz@gmail.com>
2023-10-06 21:43:19 -0600

    Se hacen cambios en matriz

--- Lab#4/matriz.sv
+++ Lab#4/matriz.sv
@@ -3,28 +3,23 @@ module matriz (
   input wire reset
 );
 
-// Registros para almacenar la matriz de enteros de 8x8
-reg [31:0] matrix[7:0][7:0];
+  // Registros para almacenar la matriz de enteros de 8x8
+  reg [9:0] matrix[7:0][7:0];
 
-always @(posedge clk or posedge reset) begin
-  if (reset) begin
-    // Inicialización de la matriz en el flanco de reset
-    for (int i = 0; i < 8; i = i + 1) begin
-      for (int j = 0; j < 8; j = j + 1) begin
-        // Asignar valores específicos a elementos de la matriz
-        if (i == 1 && j == 2) matrix[i][j] <= 32'h123;
-        else if (i == 3 && j == 4) matrix[i][j] <= 32'h456;
-        else matrix[i][j] <= 32'h0; // Inicializar otros elementos a 0 u otros valores
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      // Inicialización de la matriz en el flanco de reset
+      for (int i = 0; i < 8; i = i + 1) begin
+        for (int j = 0; j < 8; j = j + 1) begin
+          matrix[i][j] <= 10'b0; // Inicializar todos los elementos a 0
+        end
       end
+    end else begin
+      // Acciones adicionales que se pueden realizar durante el funcionamiento normal
+      // Puedes agregar lógica adicional aquí
     end
-  end else begin
-    // Acciones adicionales que se pueden realizar durante el funcionamiento normal
-    // Puedes agregar lógica adicional aquí
   end
-end
 
-// El diseño puede incluir más lógica aquí
+  // El diseño puede incluir más lógica aquí
 
 endmodule
-
-
--- Lab#4/simulation/modelsim/BuscaMinas_run_msim_rtl_verilog.do.bak11
+++ Lab#4/simulation/modelsim/BuscaMinas_run_msim_rtl_verilog.do.bak11
@@ -45,11 +45,11 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -sv -work work +incdir+C:/Users/XPC/Documents/GitHub/njohnson_jmarschatz_gconejo_digital_design_lab_2023/Lab#4 {C:/Users/XPC/Documents/GitHub/njohnson_jmarschatz_gconejo_digital_design_lab_2023/Lab#4/BuscarYAsignar.sv}
+vlog -sv -work work +incdir+C:/Users/XPC/Documents/GitHub/njohnson_jmarschatz_gconejo_digital_design_lab_2023/Lab#4 {C:/Users/XPC/Documents/GitHub/njohnson_jmarschatz_gconejo_digital_design_lab_2023/Lab#4/matriz.sv}
 
-vlog -sv -work work +incdir+C:/Users/XPC/Documents/GitHub/njohnson_jmarschatz_gconejo_digital_design_lab_2023/Lab#4 {C:/Users/XPC/Documents/GitHub/njohnson_jmarschatz_gconejo_digital_design_lab_2023/Lab#4/BuscarYAsignar_tb.sv}
+vlog -sv -work work +incdir+C:/Users/XPC/Documents/GitHub/njohnson_jmarschatz_gconejo_digital_design_lab_2023/Lab#4 {C:/Users/XPC/Documents/GitHub/njohnson_jmarschatz_gconejo_digital_design_lab_2023/Lab#4/matriz_tb.sv}
 
-vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cyclonev_ver -L cyclonev_hssi_ver -L cyclonev_pcie_hip_ver -L rtl_work -L work -voptargs="+acc"  BuscarYAsignar_tb
+vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cyclonev_ver -L cyclonev_hssi_ver -L cyclonev_pcie_hip_ver -L rtl_work -L work -voptargs="+acc"  matriz_tb
 
 add wave *
 view structure
