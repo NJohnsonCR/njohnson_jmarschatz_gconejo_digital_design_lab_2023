90cc2167c51a7eed88dba74277e05055e4856a22
Naheem Johnson <60128330+NJohnsonCR@users.noreply.github.com>
2023-10-08 21:52:10 -0600

    Agregado el módulo que busca la bomba por posición X y Y

--- Lab#4/CheckMatrixValue_tb.sv
+++ Lab#4/CheckMatrixValue_tb.sv
@@ -1,55 +1,61 @@
 module CheckMatrixValue_tb;
+  reg [3:0] X_tb; // Coordenada X de 4 bits
+  reg [3:0] Y_tb; // Coordenada Y de 4 bits
+  reg [9:0] matrix_tb [7:0][7:0]; // Matriz de 10 bits (8x8)
+  wire result_tb; // Salida que indica si se encontró un 1 en la posición especificada
 
-  // Declarar señales de entrada y salida
-  reg [3:0] X;
-  reg [3:0] Y;
-  wire result;
-
-  // Instanciar el módulo CheckMatrixValue
+  // Instancia el módulo CheckMatrixValue
   CheckMatrixValue uut (
-    .X(X),
-    .Y(Y),
-    .matrix({
-      {10'b1, 10'b0, 10'b0, 10'b1, 10'b0, 10'b0, 10'b0, 10'b0},
-      {10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0},
-      {10'b0, 10'b0, 10'b1, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0},
-      {10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0},
-      {10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0},
-      {10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b1, 10'b0, 10'b0},
-      {10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0},
-      {10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b0, 10'b1}
-    }),
-    .result(result)
+    .X(X_tb),
+    .Y(Y_tb),
+    .matrix(matrix_tb),
+    .result(result_tb)
   );
 
-  // Simulación
+  // Inicializa las entradas
   initial begin
-    // Prueba 1: Coordenadas X e Y corresponden a una posición con 0
-    X = 0; Y = 0;
-    #10; // Esperar un tiempo
-    if (result != 1'b0) $display("Prueba 1 fallida");
-
-    // Prueba 2: Coordenadas X e Y corresponden a una posición con 1 (bomba)
-    X = 2; Y = 2;
-    #10; // Esperar un tiempo
-    if (result != 1'b1) $display("Prueba 2 fallida");
-	 
-	 // Prueba 2: Coordenadas X e Y corresponden a una posición con 1 (bomba)
-    X = 3; Y = 3;
-    #10; // Esperar un tiempo
-    if (result == 1'b1) $display("Encontrado");
-
-    // Imprimir posiciones con bombas
-    $display("Posiciones con bombas:");
+    // Rellenar la matriz con valores de ejemplo
     for (int i = 0; i < 8; i = i + 1) begin
       for (int j = 0; j < 8; j = j + 1) begin
-        if (uut.matrix[i][j][0] == 1'b1) begin
-          $display("Posición (%d, %d)", i, j);
-        end
+        matrix_tb[i][j] = $random; // Rellenar con valores aleatorios de 10 bits
       end
     end
+	 
+    // Imprimir la matriz en binario
+    $display("Matriz (en binario):");
+    for (int i = 0; i < 8; i = i + 1) begin
+      for (int j = 0; j < 8; j = j + 1) begin
+        $write("%b ", matrix_tb[i][j]);
+      end
+      $write("\n");
+    end
+	 
+    // Prueba 1: Verificar si hay bomba en la posición (0,0)
+    X_tb = 0;
+    Y_tb = 0;
+    #10; // Espera un corto período de tiempo
+    $display("Prueba 1: Resultado = %b", result_tb);
 
-    $finish; // Finalizar la simulación
+    // Prueba 2: Verificar si hay bomba en la posición (2,1)
+    X_tb = 2;
+    Y_tb = 1;
+    #10; // Espera un corto período de tiempo
+    $display("Prueba 2: Resultado = %b", result_tb);
+
+    // Prueba 3: Verificar si hay bomba en la posición (7,7)
+    X_tb = 7;
+    Y_tb = 7;
+    #10; // Espera un corto período de tiempo
+    $display("Prueba 2: Resultado = %b", result_tb);
+	 
+	 // Prueba 4: Verificar si hay bomba en la posición (6,7)
+    X_tb = 6;
+    Y_tb = 7;
+    #10; // Espera un corto período de tiempo
+    $display("Prueba 2: Resultado = %b", result_tb);
+
+
+
+    $finish; // Termina la simulación
   end
-
 endmodule
